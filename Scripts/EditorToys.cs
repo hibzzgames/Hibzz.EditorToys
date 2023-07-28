2dc25bfe54b2fead833800733dc7fa08de41d84e
Hibnu Hishath <hibnuhishath1@gmail.com>
2023-07-29 05:09:02 +0530

    Added update system
    Keeps track of the last time it was called to calculate the delta time of the editor... This can be used by many other systems

---
+++ Scripts/EditorToys.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hibzz
+{
+    public static partial class EditorToys
+    {
+        /// <summary>
+        /// The last time since startup of the application... Useful to calculate editor delta time
+        /// </summary>
+        private static double lastTimeSinceStartup;
+
+        /// <summary>
+        /// Get the delta time on the editor
+        /// </summary>
+        public static float EditorDeltaTime { get; private set; }
+
+        /// <summary>
+        /// Initialize the print queue system
+        /// </summary>
+        [InitializeOnLoadMethod]
+        private static void InitializeEditorToys()
+        {
+            lastTimeSinceStartup = EditorApplication.timeSinceStartup;
+            EditorApplication.update += EditorToysUpdate;
+        }
+
+        /// <summary>
+        /// Called every frame on the editor
+        /// </summary>
+        private static void EditorToysUpdate()
+        {
+            // calculate the editor delta time
+            EditorDeltaTime = (float) (EditorApplication.timeSinceStartup - lastTimeSinceStartup);
+            lastTimeSinceStartup = EditorApplication.timeSinceStartup;
+
+            // call update for other editor tools
+            PrintQueueUpdate();
+        }
+    }
+}
