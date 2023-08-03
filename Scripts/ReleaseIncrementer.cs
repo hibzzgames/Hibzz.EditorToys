0d53d485a66968db99a7f1005f3634d0be860b70
Hibnu Hishath <hibnuhishath1@gmail.com>
2023-08-04 01:04:13 +0530

    Added option to enable/disable release incrementor

--- Scripts/ReleaseIncrementer.cs
+++ Scripts/ReleaseIncrementer.cs
@@ -2,9 +2,6 @@
 
 #if UNITY_EDITOR
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -16,35 +13,44 @@ namespace Hibzz
     {
         internal class ReleaseIncrementer : IPreprocessBuildWithReport
         {
+            const string MENU_KEY = "Hibzz/Editor Toys/Release Incrementer";
+
             public int callbackOrder => 0;
 
             public void OnPreprocessBuild(BuildReport report)
             {
-                // TODO: add a boolean toggle to enable/disable this tool
+                // only execute the tool's code if it was enabled
+                if (!Menu.GetChecked(MENU_KEY)) { return; }
 
                 // Ask the user if they want to increment the release version
                 // A response of "1" means the user decided to not increment the release version
                 int dialog_response = EditorUtility.DisplayDialogComplex("Editor Toys", "Increment the build version?", "Minor", "None", "Patch");
-                if(dialog_response == 1) { return; }
+                if (dialog_response == 1) { return; }
 
                 // the user wants to increment the minor/patch version
                 string version_string = PlayerSettings.bundleVersion;
                 Version version = Version.Parse(version_string);
 
                 // the user has requested a minor increment
-                if(dialog_response == 0)
+                if (dialog_response == 0)
                 {
                     version = new Version(version.Major, version.Minor + 1, 0);
                 }
 
                 // the user has requested a patch increment
-                else if(dialog_response == 2)
+                else if (dialog_response == 2)
                 {
                     version = new Version(version.Major, version.Minor, version.Build + 1);
                 }
 
                 // convert the version back to string and update the bundle version on the player settings
                 PlayerSettings.bundleVersion = version.ToString();
+            }
+
+            [MenuItem(MENU_KEY)]
+            static void ToggleReleaseIncrementer()
+            {
+                Menu.SetChecked(MENU_KEY, !Menu.GetChecked(MENU_KEY));
             }
         }
     }
